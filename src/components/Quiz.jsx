1fbf7e57316c2838cd8b50996506e169f0b45df8
MaksZiem <mtziembicki@gmail.com>
2024-04-03 10:27:33 +0200

    adding quiz complete

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react"
 
 import QUESTIONS from '../questions'
+import quizComplete from '../assets/quiz-complete.png'
 
 export default function Quiz() {
 
     const [userAnswers, setUsersAnswers] = useState([])
 
     const activeQuestionIndex = userAnswers.length
+    
+    const quizIsComplete = activeQuestionIndex === QUESTIONS.length
 
     function handleSelectAnswer(selectedAnswer) {
         setUsersAnswers((prevUserAnswers) => {
@@ -14,12 +17,24 @@ export default function Quiz() {
         })
     }
 
+    if(quizIsComplete) {
+        return (
+            <div id="summary">
+                <img src={quizComplete} alt="ttt" />
+                <h2>Quiz complete</h2>
+            </div>
+        )
+    }
+
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers]
+    shuffledAnswers.sort(() => Math.random() - 0.5)
+
     return (
         <div id="quiz">
             <div id="question">
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
-                    {QUESTIONS[activeQuestionIndex].answers.map((answer) =>
+                    {shuffledAnswers.map((answer) =>
                         <li key={answer} className="answer"><button onClick={() => handleSelectAnswer(answer)}>{answer}</button></li>)}
                 </ul>
             </div>
